36d3fd168394f41a809ba1a61d93e0d3638d1ac7
rfunk82 <rfunk82@gmail.com>
2013-11-25 10:41:53 -0500

    Initial Commit

---
+++ RFPasswordGenerator/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  RFPasswordGenerator
+//
+//  Created by Ricardo Funk on 11/25/13.
+//  Copyright (c) 2013 Ricardo Funk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ RFPasswordGenerator/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  RFPasswordGenerator
+//
+//  Created by Ricardo Funk on 11/25/13.
+//  Copyright (c) 2013 Ricardo Funk. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
